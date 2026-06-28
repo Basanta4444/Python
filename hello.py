14a6766690cc98560695a0ce81eb7a0c1743843d
Basanta Khadka <basantakhadka8484@gmail.com>
2026-06-28 10:04:05 +0545

    my first commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+# print("Hello world")
+# print("*" * 10)
+x = 1
+y = 2
+unit_price = 3
+print(x + y * unit_price)
+print(x)
+course = "Python"
+print(len(course))
+print(course[0:3])
+course = "Python \nProgramming"
+print(course)
+first = "John"
+last = "Smith"
+full = first + " " + last
+full = f"{first} {last}"
+print(full)
